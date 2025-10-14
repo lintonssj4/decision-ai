f84dacd965e54da3e7b7b1dd1a68e809a6abea00
lintonssj4 <lintonssj4@gmail.com>
2025-10-14 11:55:03 -0400

    Mudanças 2

--- src/modules/option/dto/create-option.dto.ts
+++ src/modules/option/dto/create-option.dto.ts
@@ -1,5 +1,10 @@
+import { IsString } from 'class-validator';
+
 export class CreateOptionDto {
+  @IsString()
   name: string;
+  @IsString()
   description: string;
+  @IsString()
   scenarioId: string;
 }
--- src/modules/scenario/dto/create-scenario.dto.ts
+++ src/modules/scenario/dto/create-scenario.dto.ts
@@ -1,4 +1,8 @@
+import { IsString } from 'class-validator';
+
 export class CreateScenarioDto {
+  @IsString()
   title: string;
+  @IsString()
   description: string;
 }
