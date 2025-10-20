d3a6a12bab0bf0e894f2c688072a47dcb743bb47
lintonssj4 <lintonssj4@gmail.com>
2025-10-20 11:02:23 -0400

    inicio score

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { ScenarioModule } from './modules/scenario/scenario.module';
 import { OptionModule } from './modules/option/option.module';
 import { CriterionModule } from './modules/criterion/criterion.module';
+import { ScoreModule } from './modules/score/score.module';
 
 @Module({
-  imports: [ScenarioModule, OptionModule, CriterionModule],
+  imports: [ScenarioModule, OptionModule, CriterionModule, ScoreModule],
   controllers: [AppController],
   providers: [AppService],
 })
